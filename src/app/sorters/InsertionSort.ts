bd5d8b195b20b17809baaf4fb90028727dfcaf13
GonQue <gonsanke@outlook.com>
2020-08-22 13:01:10 +0100

    Insertion sort backwards bug fixed

--- src/app/sorters/InsertionSort.ts
+++ src/app/sorters/InsertionSort.ts
@@ -1,27 +1,20 @@
 import {Sorter} from "./Sorter";
-import {Transition} from "../components/transition";
 import {Frame} from "../components/frame";
 import {Bar} from "../components/bar";
 
 export class InsertionSort extends Sorter {
   sort(array: Bar[], l: number, r: number) : Frame[] {
     let i, frames = [], frameArray = this.copy(array);
-
     frames.push(new Frame(array, [], false));
 
     for (i = r; i > l; i--) {
       frameArray = this.copy(frameArray);
-      //transitions.push(new Transition(i, i - 1, 'comparing', false));
       frameArray[i].state = 'comparing';
       frameArray[i - 1].state = 'comparing';
       frames.push(new Frame(frameArray, [i, i - 1], false));
-
       frameArray = this.copy(frameArray);
 
       if (this.less(frameArray[i], frameArray[i - 1])) {
-
-        // transitions.push(new Transition(i - 1, i, 'swap', false));
-        // transitions.push(new Transition(i - 1, -1, 'minimum', false))
         frameArray[i].state = 'minimum';
         frameArray[i - 1].state = 'initial';
         frames.push(new Frame(frameArray, [i, i - 1], false));
@@ -30,7 +23,6 @@ export class InsertionSort extends Sorter {
         this.swap(frameArray, i - 1, i);
         frameArray[i - 1].state = 'minimum';
         frameArray[i].state = 'initial';
-
       }
       else {
         frameArray[i - 1].state = 'minimum';
@@ -38,59 +30,51 @@ export class InsertionSort extends Sorter {
       }
       frames.push(new Frame(frameArray, [i, i - 1], false));
     }
-    //transitions.push(new Transition(0, -1, 'sorted', false));
     frameArray = this.copy(frameArray);
     frameArray[0].state = 'sorted';
     frames.push(new Frame(frameArray, [0], false));
+    frameArray = this.copy(frameArray);
 
     let lastJ = 1;
 
     for (i = l + 2; i <= r; i++) {
       let j = i, v = frameArray.slice(i)[0], changes = [];
-      frameArray = this.copy(frameArray);
 
       frameArray[lastJ].state = 'initial';
       frameArray[i].state = 'highlight';
       frames.push(new Frame(frameArray, [i, lastJ], false));
       frameArray = this.copy(frameArray);
-
-      changes.push(i);
+      changes.push(i); // to change last highlight to initial
 
       while (this.less(v, frameArray[j - 1])) {
-        //transitions.push(new Transition(i, j - 1, 'comparing', false));
-        //frameArray[i].state = 'comparing';
         frameArray[j - 1].state = 'comparing';
         changes.push(j - 1);
 
-        frames.push(new Frame(frameArray, [j - 1], false));
+        // j in changes to have only one compare animation
+        frames.push(new Frame(frameArray, [j - 1, j], false));
         frameArray = this.copy(frameArray);
 
-        //frameArray[i].state = 'initial';
+        // comment to have multiple bar compare animation
         frameArray[j - 1].state = 'initial';
 
         frameArray[j] = frameArray[j - 1];
-        //frames.push(new Frame(frameArray, [j], false));
-        //frameArray = this.copy(frameArray);
-        // let transition = new Transition(j, frameArray[j - 1].height, 'set', false);
-        // transition.initialHeight = frameArray[j].height;
-        // transitions.push(transition);
-        // transitions.push(new Transition(i, j - 1, 'initial', false));
         j--;
       }
+
+      // uncomment to have multiple bar compare animation
+      //changes.forEach(i => frameArray[i].state = 'initial');
+
       changes.push(j);
       frameArray[j] = v;
       frameArray[j].state = 'highlight';
       frames.push(new Frame(frameArray, changes, false));
+      frameArray = this.copy(frameArray);
       lastJ = j;
-      // let transition = new Transition(j, v.height, 'set', false);
-      // transition.initialHeight = frameArray[j].height;
-      // transitions.push(transition);
     }
     frameArray = this.copy(frameArray);
     let changes = [];
     for (i = 1; i <= r; i++) {
       changes.push(i);
-      // transitions.push(new Transition(i, -1, "sorted", false));
       frameArray[i].state = 'sorted';
     }
     frames.push(new Frame(frameArray, changes, true));
--- src/app/sorters/SelectionSort.ts
+++ src/app/sorters/SelectionSort.ts
@@ -5,26 +5,21 @@ import {Bar} from "../components/bar";
 export class SelectionSort extends Sorter{
   sort(array: Bar[], l: number, r: number) : Frame[] {
     let i, j, frames = [], frameArray = this.copy(array);
-    //maybe all change
     frames.push(new Frame(array, [], false ));
     for (i = l; i < r; i++) {
       let min = i;
       frameArray = this.copy(frameArray);
-      //transitions.push(new Transition(i, -1, 'minimum', false));
       frameArray[i].state = 'minimum';
       frames.push(new Frame(frameArray, [i], false));
       for (j = i + 1; j <= r; j++) {
         let changes;
         frameArray = this.copy(frameArray);
-        //transitions.push(new Transition(j, -1, 'comparing', false));
         frameArray[j].state = 'comparing';
         frameArray[min].state = 'comparing';
         frames.push(new Frame(frameArray, [j, min], false));
 
         frameArray = this.copy(frameArray);
         if (this.less(frameArray[j], frameArray[min])) {
-          //transitions.push(new Transition(min, -1, 'initial', false));
-          //transitions.push(new Transition(j, -1, 'minimum', false));
           frameArray[min].state = 'initial';
           changes = [min, j];
           min = j;
@@ -33,8 +28,6 @@ export class SelectionSort extends Sorter{
         else {
           frameArray[min].state = 'minimum';
           frameArray[j].state = 'initial';
-          //transitions.push(new Transition(j, -1, 'initial', false));
-          //transitions.push(new Transition(min, -1, 'minimum'));
           changes = [min, j];
         }
         frames.push(new Frame(frameArray, changes, false));
@@ -43,25 +36,17 @@ export class SelectionSort extends Sorter{
       console.log('before:' + frameArray[i].height + " ; " + frameArray[min].height);
       this.swap(frameArray, i, min);
       console.log('after:' + frameArray[i].height + " ; " + frameArray[min].height);
-      //transitions.push(new Transition(i, min, 'swap', false));
-      // frames.push(new Frame(frameArray, [i, min], false));
-      //
-      // frameArray = this.copy(frameArray);
 
       if (i === r - 1)  { // last iteration, everything is sorted
-        //transitions.push(new Transition(i, i + 1, 'sorted', false));
         frameArray[i].state = 'sorted';
         frameArray[i + 1].state = 'sorted';
         frames.push(new Frame(frameArray, [i, i + 1], true));
       }
       else {
-        // transitions.push(new Transition(min, -1, 'initial', false));
-        // transitions.push(new Transition(i, -1, 'sorted', false));
         frameArray[min].state = 'initial';
         frameArray[i].state = 'sorted';
         frames.push(new Frame(frameArray, [i, min], false));
       }
-
     }
     frames[frames.length - 1].array.forEach(element => console.log(element.height));
     console.log('There are ' + frames.length + ' frames.');

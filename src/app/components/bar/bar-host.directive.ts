a9e62bb1f391e9a3f21f04f2797c73000e3d6b8d
GonQue <32230042+GonQue@users.noreply.github.com>
2020-09-18 00:49:33 +0000

    Deploying to gh-pages from  @ b3a504ce8d74be6c0651b88af88293d82465be8d 🚀

--- src/app/components/bar/bar-host.directive.ts
+++
@@ -1,8 +0,0 @@
-import {Directive, ViewContainerRef} from '@angular/core';
-
-@Directive({
-  selector: '[bar-host]'
-})
-export class BarDirective {
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
